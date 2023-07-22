1586ff3447c00e4742aaa9b8272b093f039cb0b4
Tendzin ROFFLER <40204866+Glassait@users.noreply.github.com>
2023-07-22 19:27:50 +0200

    Fix Issue 4 (#5)
    #major

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.use(express.static(__dirname + "/dist/app"));
 app.get("/*", function (_req, res) {
     res.sendFile(path.join(__dirname + "/dist/app/index.html"));
