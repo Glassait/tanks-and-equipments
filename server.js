470f4288db2b1e7614c428a1bdf7d9cfd61a276e
Glassait <tendzinroffler@gmail.com>
2023-07-31 20:37:38 +0200

    feat(1.1.3): Ajout/Modification chars

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// set up rate limiter: maximum of threnty requests per minute
+// set up rate limiter: maximum of sixty requests per minute
 var RateLimit = require('express-rate-limit');
 var limiter = RateLimit({
   windowMs: 1*60*1000, // 1 minute
-  max: 50
+  max: 60
 });
 
 // apply rate limiter to all requests
