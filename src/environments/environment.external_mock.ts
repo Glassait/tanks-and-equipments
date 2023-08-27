637e6989674ccc6e18e52f111d759b3f8c243de2
Glassait <tendzinroffler@gmail.com>
2023-08-27 16:07:45 +0200

    feat(1.2.0): Modifie environment config

--- src/environments/environment.external_mock.ts
+++ src/environments/environment.external_mock.ts
@@ -1,6 +1,6 @@
 import { MockEnum } from '../app/commons/enums/mock.enum';
 
 export const environment = {
-    production: true,
-    mock: MockEnum.NO_MOCK,
+    production: false,
+    mock: MockEnum.EXTERNAL_MOCK,
 };
