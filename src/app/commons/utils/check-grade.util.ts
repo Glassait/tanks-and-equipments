425db736dfadf5a6759d0b1ad7539aa9c316ed4f
glassait <tendzinroffler@gmail.com>
2024-01-14 16:59:12 +0100

    Remove combat officier

--- src/app/commons/utils/check-grade.util.ts
+++ src/app/commons/utils/check-grade.util.ts
@@ -1,11 +1,16 @@
 import { GradeEnum } from '../enums/grade.enum';
 
 export class CheckGrade {
+    /**
+     * Checks if the given grade is an admin grade
+     * @param grade The grade to check
+     * @returns Whether the given grade is an admin grade
+     */
     public static isAdmin(grade: GradeEnum | string | undefined): boolean {
         if (!grade) {
             return false;
         }
 
-        return [GradeEnum.COMBAT_OFFICER, GradeEnum.COMMANDER, GradeEnum.EXECUTIVE_OFFICER].includes(grade as GradeEnum);
+        return [GradeEnum.COMMANDER, GradeEnum.EXECUTIVE_OFFICER].includes(grade as GradeEnum);
     }
 }
