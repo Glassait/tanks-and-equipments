6d2580b0bf73d35a98f9f058ddbf53fd7993ddb8
glassait <tendzinroffler@gmail.com>
2024-01-14 15:06:59 +0100

    Update admin requirement

--- src/app/commons/utils/check-grade.util.ts
+++ src/app/commons/utils/check-grade.util.ts
@@ -6,13 +6,6 @@ export class CheckGrade {
             return false;
         }
 
-        return [
-            GradeEnum.COMBAT_OFFICER,
-            GradeEnum.COMMANDER,
-            GradeEnum.EXECUTIVE_OFFICER,
-            GradeEnum.INTELLIGENCE_OFFICER,
-            GradeEnum.PERSONNEL_OFFICER,
-            GradeEnum.RECRUITMENT_OFFICER,
-        ].includes(grade as GradeEnum);
+        return [GradeEnum.COMBAT_OFFICER, GradeEnum.COMMANDER, GradeEnum.EXECUTIVE_OFFICER].includes(grade as GradeEnum);
     }
 }
