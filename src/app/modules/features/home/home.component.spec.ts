3f2d3ced13f5eea99ee645fe2cfbc611231983ff
glassait <tendzinroffler@gmail.com>
2024-10-08 19:47:48 +0200

    Rework karma config

--- src/app/modules/features/home/home.component.spec.ts
+++ src/app/modules/features/home/home.component.spec.ts
@@ -2,21 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 
-describe('HomeComponent', () => {
+describe('HomeComponent', (): void => {
     let component: HomeComponent;
     let fixture: ComponentFixture<HomeComponent>;
 
-    beforeEach(async () => {
+    beforeEach(async (): Promise<void> => {
         await TestBed.configureTestingModule({
             imports: [HomeComponent],
         }).compileComponents();
+    });
 
+    beforeEach((): void => {
         fixture = TestBed.createComponent(HomeComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    it('should create', () => {
+    it('should create', (): void => {
         expect(component).toBeTruthy();
     });
 });
