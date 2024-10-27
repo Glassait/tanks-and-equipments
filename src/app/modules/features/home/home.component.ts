ae7c3c34e76af544511522ce96cf6a2348866bbb
glassait <tendzinroffler@gmail.com>
2024-10-27 18:36:50 +0100

    Section intro

--- src/app/modules/features/home/home.component.ts
+++ src/app/modules/features/home/home.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
-import { FoldNewsCardComponent } from 'fold';
+import { ChangeDetectionStrategy, Component, HostBinding, inject, makeStateKey, type OnInit, PLATFORM_ID } from '@angular/core';
+import { FoldLinkDirective, FoldNewsCardComponent, FoldTankCardComponent, FoldTextComponent } from 'fold';
+import { TanksOverviewProxy } from '../../shared/proxy/tanks-overview.proxy';
+import { isPlatformBrowser, isPlatformServer, JsonPipe, NgClass } from '@angular/common';
+import { TransferState } from '@angular/platform-browser';
+import type { TankOverview } from 'generated-api/tank';
+import { PathEnum } from 'core/enums/path.enum';
+
+const dataKey = makeStateKey<{ data: string }>('data');
 
 @Component({
     selector: 'home',
@@ -7,11 +14,40 @@ import { FoldNewsCardComponent } from 'fold';
     styleUrl: './home.component.scss',
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [FoldNewsCardComponent],
+    imports: [FoldNewsCardComponent, JsonPipe, FoldTextComponent, FoldLinkDirective, FoldTankCardComponent, NgClass],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
     @HostBinding('class')
     get cssClasses(): string[] {
         return ['fold-grid'];
+    }
+
+    //region INJECTION
+    private readonly tanksOverviewService: TanksOverviewProxy = inject(TanksOverviewProxy);
+    private readonly platformId = inject(PLATFORM_ID);
+    private readonly transferState = inject(TransferState);
+    //endregion
+
+    protected readonly PathEnum = PathEnum;
+
+    protected tanksOverview: TankOverview[] = [];
+
+    ngOnInit(): void {
+        if (isPlatformServer(this.platformId)) {
+            this.tanksOverviewService.tanksOverview().subscribe({
+                next: (tankOverviews: TankOverview[]): void => {
+                    this.tanksOverview = tankOverviews.filter(({ priority }): boolean => priority === 5);
+                    this.transferState.set(dataKey, this.tanksOverview);
+                },
+                error: err => {
+                    console.error(err);
+                },
+            });
+        }
+
+        if (isPlatformBrowser(this.platformId)) {
+            this.tanksOverview = this.transferState.get(dataKey);
+            console.log(this.tanksOverview);
+        }
     }
 }
