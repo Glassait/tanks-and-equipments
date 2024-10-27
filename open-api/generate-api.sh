5ca7070e417ad22d436d3d4add1a55bd704b7d8a
glassait <tendzinroffler@gmail.com>
2024-10-27 14:33:35 +0100

    Generation de l'api tanks

---
+++ open-api/generate-api.sh
@@ -0,0 +1,4 @@
+echo "Delete generated api"
+rm -rf ./src/generated-api
+
+./open-api/tanks/generate-tanks-api.sh
---
+++ open-api/tanks/generate-tanks-api.sh
@@ -0,0 +1,2 @@
+echo "Start generating TANKS api"
+npm run open-api -- -i ./open-api/tanks/tanks.yml -o src/generated-api/tank
