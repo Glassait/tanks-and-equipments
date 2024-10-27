e51c9dd4eef4886c77ad6ee56c1447427776d8a9
glassait <tendzinroffler@gmail.com>
2024-10-27 17:49:38 +0100

    Fix component

--- projects/fold/src/components/molecule/tank-card/fold-tank-card.component.ts
+++ projects/fold/src/components/molecule/tank-card/fold-tank-card.component.ts
@@ -18,36 +18,24 @@ import { ReplacePipe } from '../../../pipes/replace/replace.pipe';
 export class FoldTankCardComponent implements OnInit {
     public static id: number = 0; // NOSONAR
 
-    /**
-     * TODO change type to nation enum when generated api
-     */
     public nation: InputSignal<string> = input.required();
 
     public name: InputSignal<string> = input.required();
 
-    public wotTankName: InputSignal<string> = input.required();
+    public wotName: InputSignal<string> = input.required();
 
-    /**
-     * TODO change type to nation enum when generated api
-     */
     public type: InputSignal<string> = input.required();
 
     public level: InputSignal<number> = input.required();
 
-    /**
-     * TODO change type to nation enum when generated api
-     */
     public role: InputSignal<string> = input.required();
 
-    /**
-     * TODO change type to nation enum when generated api
-     */
     public priority: InputSignal<number> = input.required();
 
-    public isReward: InputSignal<boolean> = input.required();
+    public isReward: InputSignal<boolean | undefined> = input();
 
     protected tankImageUrl: Signal<string> = computed(
-        (): string => `https://eu-wotp.wgcdn.co/dcont/tankopedia_images/${this.wotTankName()}/${this.wotTankName()}_image_resized.png`
+        (): string => `https://eu-wotp.wgcdn.co/dcont/tankopedia_images/${this.wotName()}/${this.wotName()}_image_resized.png`
     );
 
     protected flagImageUrl: Signal<string> = computed((): string => `images/nation/flag-${this.nation().toLowerCase()}.png`);
