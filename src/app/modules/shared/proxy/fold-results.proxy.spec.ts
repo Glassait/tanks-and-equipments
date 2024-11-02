93a0d8bc4a5d87d967b16a3cbb59bbea4f93366a
glassait <tendzinroffler@gmail.com>
2024-11-02 11:22:30 +0100

    Add service test

---
+++ src/app/modules/shared/proxy/fold-results.proxy.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FoldResultService } from 'generated-api/fold';
+import { FoldResultsProxy } from 'shared/proxy/fold-results.proxy';
+import { of } from 'rxjs';
+
+describe('Fold Results Proxy', () => {
+    let service: FoldResultsProxy;
+
+    let generatedService: jasmine.SpyObj<FoldResultService>;
+
+    beforeEach(async () => {
+        generatedService = jasmine.createSpyObj('FoldResultService', ['foldResult']);
+
+        await TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                {
+                    provide: FoldResultService,
+                    useValue: generatedService,
+                },
+            ],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        service = TestBed.inject(FoldResultsProxy);
+    });
+
+    it('should call the generated service', () => {
+        generatedService.foldResult.and.returnValue(of());
+        expect(service.foldResults()).toBeDefined();
+        expect(generatedService.foldResult).toHaveBeenCalledTimes(1);
+    });
+});
